0090c1871f87dbed8214a55f0ca8b518e53d97bd
lfbsdev <lfbsdev@gmail.com>
2021-03-03 03:23:49 -0300

    added dynamic tick intervals, slow values

--- d2_info/spiders/d2_info_spider.py
+++ d2_info/spiders/d2_info_spider.py
@@ -8,8 +8,21 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         reduced = response.selector.xpath("//*[@id='mw-content-text']/div/div[6]/div[3]/div/ul/li/a/text()").getall()
+        dynamic = response.selector.xpath("//*[@id='mw-content-text']/div/div[6]/div[4]/div/ul/li/a/text()").getall()
+        slow = response.selector.xpath("//*[@id='mw-content-text']/div/div[6]/div[5]/div/ul/li/a/text()").getall()
+
         with open("reduced.txt", "w") as f:
             for i in range(0,len(reduced), 2):
                 s = reduced[i] + " , " + reduced[i + 1] + "\n"
+                f.write(s)
+
+        with open("dynamic.txt", "w") as f:
+            for i in range(0,len(dynamic), 2):
+                s = dynamic[i] + " , " + dynamic[i + 1] + "\n"
+                f.write(s)
+
+        with open("slow.txt", "w") as f:
+            for i in range(0,len(slow), 2):
+                s = slow[i] + " , " + slow[i + 1] + "\n"
                 f.write(s)
         
