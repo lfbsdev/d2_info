d2bb3c90dc2cd10e1739d0f547b58138f6fd3169
lfbsdev <lfbsdev@gmail.com>
2021-03-04 01:34:59 -0300

    added slow, dynamic intefaces in app

--- app.py
+++ app.py
@@ -5,5 +5,35 @@ process = CrawlerProcess(get_project_settings())
 
 # 'followall' is the name of one of the spiders of the project.
 process.crawl('d2_info')
-print("oi :)")
 process.start() # the script will block here until the crawling is finished
+
+enemies = input("Type your enemies: ").split()
+enemies = [enemy.capitalize() for enemy in enemies]
+allies = input("Type your allies: ").split()
+allies = [ally.capitalize() for ally in allies]
+
+print("Enemies:\n")
+
+print("\tReduced Duration:\n")
+with open("reduced.txt", "r") as f:
+    for line in f.readlines():
+        for enemy in enemies:
+            if (enemy in line):
+                print("\t", line, end = '')
+print("\n")
+
+print("\tReduced Slow:\n")
+with open("slow.txt", "r") as f:
+    for line in f.readlines():
+        for enemy in enemies:
+            if (enemy in line):
+                print("\t", line, end = '')
+print("\n")
+
+print("\tDynamic Tick Intervals:\n")
+with open("dynamic.txt", "r") as f:
+    for line in f.readlines():
+        for enemy in enemies:
+            if (enemy in line):
+                print("\t", line, end = '')
+print("\n")
